bfa5436e3b96766d81364fbff98f4f4ec47365d8
nasirkrmzz <nadolin.cs42@gmail.com>
2025-03-01 17:34:56 +0300

    unutulan bir kaç yapı eklendi .

--- Queue/Iqueue.cs
+++ Queue/Iqueue.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Algorithms_Data_structures.Queue
 {
-    internal class Iqueue
+    public interface IQueue<T> : IEnumerable<T>
     {
+        int Size();
+        void Enqueue(T item);
+        T Dequeue();
+        bool Contains(T item);
+        T Access(int position);
     }
+
 }
--- Queue/ListQueue.cs
+++ Queue/ListQueue.cs
@@ -6,7 +6,65 @@ using System.Threading.Tasks;
 
 namespace Algorithms_Data_structures.Queue
 {
-    internal class ListQueue
+    public class ListQueue<T> : IQueue<T>
     {
+        List<T> data;
+
+        public ListQueue(int capacity)
+        {
+            data = new List<T>(capacity);
+        }
+
+        public ListQueue()
+        {
+            data = new List<T>();
+        }
+
+        public int Size()
+        {
+            return data.Count;
+        }
+
+        public void Enqueue(T item)
+        {
+            data.Add(item);
+        }
+
+        public T Dequeue()
+        {
+            if (data.Count == 0)
+            {
+                throw new InvalidOperationException("queue is empty");
+            }
+            T itemToDelete = data.ElementAt(0);
+            data.RemoveAt(0);
+            return itemToDelete;
+        }
+
+        public T Access(int position)
+        {
+            if (data.Count == 0)
+                throw new InvalidOperationException("queue is empty");
+
+            if (position >= data.Count)
+                throw new InvalidOperationException($"no item found in the queue at given position {position}");
+
+            return data.ElementAt(position);
+        }
+
+        public bool Contains(T item)
+        {
+            return data.Contains(item);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return data.GetEnumerator();
+        }
     }
 }
